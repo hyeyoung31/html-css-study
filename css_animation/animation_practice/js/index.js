ce507f423f609877accf4faaee5270963444c3c1
hyeyoung31 <154103645+hyeyoung31@users.noreply.github.com>
2024-03-02 23:49:07 +0900

    first commit
    product list hover animation

---
+++ css_animation/animation_practice/js/index.js
@@ -0,0 +1,39 @@
+$(function () {
+
+    const tabLink = $('.tabs li a');
+    const nestedLink = $('.nested_tabs li a'); 
+  
+    tabLink.on("click", function (e) {
+      e.preventDefault();
+      const target = $(this).attr('href');
+  
+      tabLink.removeClass('active');
+      $(this).addClass('active');
+      $(target).addClass('active').siblings('.active').removeClass('active');
+  
+      const nestedLinkActive = $(target).find('.nested_tabs li a.active');
+  
+      if (!nestedLinkActive.length) {
+        $(target).find('.nested_tabs li:first-child a').trigger('click');
+      } else {
+        nestedLinkActive.trigger('click');
+      }
+    });
+  
+    nestedLink.on("click", function (e) {
+      e.preventDefault();
+      const target = $(this).attr("href");
+      nestedLink.removeClass('active');
+      $(this).addClass('active');
+      $(target).addClass('active').siblings('.active').removeClass("active");
+    });
+  
+    const tabLinkActive = tabLink.filter('.active');
+    if (!tabLinkActive.length) {
+      tabLink.first().trigger('click');
+    } else {
+      tabLinkActive.trigger('click');
+    }
+  
+  });
+  
